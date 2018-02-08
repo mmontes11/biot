4d79dc8eaee464b5798f391ec039975a5915aa8f
mmontes11 <martin11lrx@gmail.com>
2018-02-08 19:51:49 +0100

    Removed unnecesary test

--- src/routers/indexRouter.js
+++ src/routers/indexRouter.js
@@ -1,13 +1,10 @@
 import express from 'express';
 import httpStatus from 'http-status';
-import telegramBotController from "../controllers/telegramBotController";
-
 
 const router = express.Router();
 
 router.get('/health-check', (req, res) => {
     res.sendStatus(httpStatus.OK);
-    telegramBotController.bot.sendMessage(565598, "MESSAGE FROM REST")
 });
 
 export default router;
